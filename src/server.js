b6d09422eaf3e9c5b399b821db4727f32f8c6105
Андрій <gapeev1979@gmail.com>
2025-11-29 20:02:03 +0200

    start routes

--- src/server.js
+++ src/server.js
@@ -2,14 +2,12 @@
 import express from 'express';
 import cors from 'cors';
 import pino from 'pino-http';
-
 import 'dotenv/config';
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
 
-// Middleware для парсингу JSON
-app.use(express.json());
+app.use(express.json()); // Middleware для парсингу JSON
 app.use(cors()); // Дозволяє запити з будь-яких джерел
 app.use(
   pino({
@@ -28,32 +26,39 @@ app.use(
   }),
 );
 
-// Логування часу
-app.use((req, res, next) => {
-  console.log(`Time: ${new Date().toLocaleString()}`);
-  next();
-});
-
 // Маршрут
 app.get('/', (req, res) => {
   res.status(200).json({ message: 'Hello, World!' });
 });
 
-app.post('/users', (req, res) => {
-  console.log(req.body); // тепер тіло доступне як JS-об`єкт
-  res.status(201).json({ message: 'User created' });
+app.get('/gallery-paintings', (req, res) => {
+  res.status(200).json({
+    message: 'Отримано всі зображення картин.',
+  });
 });
+
+app.get('/gallery-paintings/:paintingId', (req, res) => {
+  const { paintingId } = req.params;
+  res.status(200).json({
+    message: `Отримано зображення картини - ${paintingId}`,
+  });
+});
+
+// app.post('/painting', (req, res) => {
+//   console.log(req.body); // тепер тіло доступне як JS-об`єкт
+//   res.status(201).json({ message: 'Картина додана!' });
+// });
 
 // Маршрут для тестування middleware помилки
 app.get('/test-error', (req, res) => {
   // Штучна помилка для прикладу
-  throw new Error('Something went wrong');
+  throw new Error('Ви зробили помилку!!!');
 });
 
 // Middleware 404 (після всіх маршрутів)
 
 app.use((req, res) => {
-  res.status(404).json({ message: 'Route not found' });
+  res.status(404).json({ message: 'Сторінку не знайдено' });
 });
 
 // Middleware для обробки помилок
@@ -70,5 +75,5 @@ app.use((err, req, res, next) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Сервер працює на порту ${PORT}`);
 });
